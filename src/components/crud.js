9a077d1d7350ef37db184f1d7b1809b09d642256
Gaboss <zeronet.gp@gmail.com>
2020-08-06 00:33:44 -0400

    Revertir cambios

--- src/components/crud.js
+++ src/components/crud.js
@@ -26,12 +26,6 @@ const CrudApp =()=> {
         setUsers(users.map((user) => (user.id === id ? updateUser : user)))
     }
 
-    const editRow = (userID) => {
-        let user = users.find(x=>x.id===userID)
-        setEdit(true)
-        setUserActual(user)
-    }
-
     return (
         <Container fluid >
             <Row className='my-3'>
