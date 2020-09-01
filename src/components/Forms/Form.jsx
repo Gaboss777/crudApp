17aaba83b2a03c18fa264d00bdaf0ec4f5aadc6a
Gabriel Palacios <zeronet.gp@gmail.com>
2020-09-01 16:02:36 -0400

    Add Fields  UserForm / DeleteUser / List (#11)
    * Add Fields  UserForm / DeleteUser / List
    
    Se agregaron los siguientes campos a los componentes List, Form y DeleteUser:
    - Email
    - Phone
    - Mensualidad
    - Serial
    - Mac Address
    
    * Corrections #1
    
    * Fixes on CRUD
    
    - List not clearing after update/edit
    - Deleting properly from list
    - Updating properly from list
    
    Co-authored-by: Rafael <Bytheway0708@gmail.com>

--- src/components/Forms/Form.jsx
+++ src/components/Forms/Form.jsx
@@ -1,9 +1,10 @@
 import React, { useState, Fragment, useEffect } from 'react';
-import { OverlayTrigger, Modal, Tooltip, Form, Row, Col, Button } from 'react-bootstrap';
+import { Modal, Form, Row, Col, Button } from 'react-bootstrap';
 import { createUser, updateUser } from '../../ducks/users';
 import { connect } from 'react-redux';
 import { faUserPlus, faUserEdit } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Alerts from '../Alerts/alerts';
 
 
 const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, selection }) => {
@@ -14,7 +15,12 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
     const [bandwidth, setBandwidth] = useState(user ? user.bandwidth : "")
     const [status, setStatus] = useState(user ? user.status : "")
     const [service, setService] = useState(user ? user.service : "")
-    const [ip, setIp] = useState(user ? user.ip : "");
+    const [ip, setIp] = useState(user ? user.ip : "")
+    const [email, setEmail] = useState(user ? user.ip : "")
+    const [phone, setPhone] = useState(user ? user.ip : "")
+    const [mensuality, setMensuality] = useState(user ? user.ip : "")
+    const [serial, setSerial] = useState(user ? user.ip : "")
+    const [mac, setMac] = useState(user ? user.ip : "")
 
 
     useEffect(() => {
@@ -26,8 +32,13 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
             setStatus(user.status)
             setService(user.service)
             setIp(user.ip)
+            setEmail(user.email)
+            setPhone(user.phone)
+            setMensuality(user.mensuality)
+            setSerial(user.serial)
+            setMac(user.mac)
         }
-    }, [user,editing])
+    }, [user, editing])
 
 
     const [valid, setValid] = useState(false)
@@ -37,13 +48,15 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
         if (form.checkValidity() === false) {
             event.stopPropagation()
         } else {
-            let newUser = { name, document, location, bandwidth, status, service, ip }
+            let newUser = { name, document, location, bandwidth, status, service, ip,mac,email,phone,serial,mensuality }
             if (!user) {
                 createUser(newUser)
+                Alerts.InfoNotify("USUARIO AGREGADO EXITOSAMENTE")
 
             }
             else {
                 updateUser({ ...newUser, id: user.id })
+                Alerts.EditNotify("USUARIO EDITADO EXITOSAMENTE")
 
             }
 
@@ -54,19 +67,19 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
 
     const formjsx = (
         <Form onSubmit={onSubmit} noValidate validated={valid}>
-            <Form.Group controlId='validation01' >
-                <Form.Label className='font-weight-bold text-uppercase' >Razon Social </Form.Label>
-                <Form.Control
-                    required
-                    type='text'
-                    placeholder='Ingrese Nombre'
-                    value={name}
-                    onChange={({ target }) => setName(target.value)}
-                />
-                <Form.Control.Feedback >Excelente!</Form.Control.Feedback>
-                <Form.Control.Feedback type='invalid'>Ingrese Razon Social</Form.Control.Feedback>
-            </Form.Group>
             <Form.Row>
+                <Form.Group as={Col} controlId='validation01' >
+                    <Form.Label className='font-weight-bold text-uppercase' >Razon Social </Form.Label>
+                    <Form.Control
+                        required
+                        type='text'
+                        placeholder='Ingrese Nombre'
+                        value={name}
+                        onChange={({ target }) => setName(target.value)}
+                    />
+                    <Form.Control.Feedback >Excelente!</Form.Control.Feedback>
+                    <Form.Control.Feedback type='invalid'>Ingrese Razon Social</Form.Control.Feedback>
+                </Form.Group>
                 <Form.Group as={Col} controlId='validation02' >
                     <Form.Label className='font-weight-bold text-uppercase' >CI/RIF</Form.Label>
                     <Form.Control
@@ -79,6 +92,9 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
                     <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
                     <Form.Control.Feedback type='invalid'>Ingrese CI o RIF</Form.Control.Feedback>
                 </Form.Group>
+            </Form.Row>
+            <Form.Row>
+
                 <Form.Group as={Col} controlID='validation03' >
                     <Form.Label className='font-weight-bold text-uppercase' >Ubicacion</Form.Label>
                     <Form.Control
@@ -91,35 +107,89 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
                     <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
                     <Form.Control.Feedback type='invalid'>Ingrese Ubicacion</Form.Control.Feedback>
                 </Form.Group>
+                <Form.Group as={Col} controlID='validation04' >
+                    <Form.Label className='font-weight-bold text-uppercase' >Telefono</Form.Label>
+                    <Form.Control
+                        required
+                        type='text'
+                        placeholder='Ingrese Telefono'
+                        value={phone}
+                        onChange={({ target }) => setPhone(target.value)}
+                    />
+                    <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
+                    <Form.Control.Feedback type='invalid'>Ingrese Telefono</Form.Control.Feedback>
+                </Form.Group>
             </Form.Row>
             <Form.Row>
+
+                <Form.Group as={Col} controlID='validation05' >
+                    <Form.Label className='font-weight-bold text-uppercase' >Mensualidad</Form.Label>
+                    <Form.Control
+                        required
+                        type='text'
+
+                        placeholder='Ingrese Mensualidad'
+                        value={mensuality}
+                        onChange={({ target }) => setMensuality(target.value)}
+                    />
+                    <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
+                    <Form.Control.Feedback type='invalid'>Ingrese Mensualidad</Form.Control.Feedback>
+                </Form.Group>
                 <Form.Group as={Col} controlID='validation04' >
                     <Form.Label className='font-weight-bold text-uppercase' >Bandwidth</Form.Label>
                     <Form.Control
                         required
                         type='number'
-                        placeholder='bandwidth'
+                        placeholder='Ingrese Ancho de Banda'
                         value={bandwidth}
                         onChange={({ target }) => setBandwidth(target.value)}
                     />
                     <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
                     <Form.Control.Feedback type='invalid'>Ingrese Ancho de Banda</Form.Control.Feedback>
                 </Form.Group>
+            </Form.Row>
+            <Form.Row>
+
                 <Form.Group as={Col} controlID='validation05' >
                     <Form.Label className='font-weight-bold text-uppercase' >Direccion IP</Form.Label>
                     <Form.Control
                         required
                         type='text'
 
-                        placeholder='ipAddress'
+                        placeholder='Ingrese Direccion IP'
                         value={ip}
                         onChange={({ target }) => setIp(target.value)}
                     />
                     <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
                     <Form.Control.Feedback type='invalid'>Ingrese Direccion IP</Form.Control.Feedback>
                 </Form.Group>
+                <Form.Group as={Col} controlID='validation04' >
+                    <Form.Label className='font-weight-bold text-uppercase' >Serial</Form.Label>
+                    <Form.Control
+                        required
+                        type='text'
+                        placeholder='Ingrese Serial'
+                        value={serial}
+                        onChange={({ target }) => setSerial(target.value)}
+                    />
+                    <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
+                    <Form.Control.Feedback type='invalid'>Ingrese Serial</Form.Control.Feedback>
+                </Form.Group>
             </Form.Row>
             <Form.Row>
+              
+                <Form.Group as={Col} controlID='validation05' >
+                    <Form.Label className='font-weight-bold text-uppercase' >Direccion MAC</Form.Label>
+                    <Form.Control
+                        required
+                        type='text'
+                        placeholder='Ingrese MAC Address'
+                        value={mac}
+                        onChange={({ target }) => setMac(target.value)}
+                    />
+                    <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
+                    <Form.Control.Feedback type='invalid'>Ingrese Mac Address</Form.Control.Feedback>
+                </Form.Group>
                 <Form.Group as={Col} controlID='validation06' >
                     <Form.Label className='font-weight-bold text-uppercase' >Servicio</Form.Label>
                     <Form.Control as='select' value={service} onChange={({ target }) => setService(target.value)} required custom >
@@ -131,7 +201,10 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
                     <Form.Control.Feedback>Excelente!</Form.Control.Feedback>
                     <Form.Control.Feedback type='invalid'>Ingrese Tipo de Servicio</Form.Control.Feedback>
                 </Form.Group>
-                <Form.Group as={Col} controlID='validation07' >
+            </Form.Row>
+            <Form.Row>
+               
+                <Form.Group as={Col} sm={6} controlID='validation07' >
                     <Form.Label className='font-weight-bold text-uppercase' >Estado</Form.Label>
                     <Form.Control as='select' value={status} onChange={({ target }) => setStatus(target.value)} required custom >
                         <option value='' disabled selected >Elija un Estado</option>
@@ -155,10 +228,10 @@ const UserForm = ({ btnText, createUser, user, updateUser, asModal, editing, sel
     if (asModal) {
         return (
             <Fragment>
-                <Button  disabled={editing ? selection.length !== 1 : false} variant='warning'  onClick={() => setShowModal(true)}><FontAwesomeIcon icon={editing ? faUserEdit : faUserPlus} size='lg' /></Button>
+                <Button disabled={editing ? selection.length !== 1 : false} variant='warning' onClick={() => setShowModal(true)}><FontAwesomeIcon icon={editing ? faUserEdit : faUserPlus} size='lg' /></Button>
                 <Modal show={showModal} onHide={() => setShowModal(false)} centered size='lg' >
-                    <Modal.Header closeButton >
-                        <Modal.Title className='text-center w-100 text-white' >Test Title</Modal.Title>
+                    <Modal.Header closeButton className='bg-warning' >
+                        <Modal.Title className='text-center w-100 text-white' >{editing ? 'Editar Usuario' : 'Agregar Usuario'}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         {formjsx}
--- src/components/users/List.jsx
+++ src/components/users/List.jsx
@@ -3,7 +3,7 @@ import { Table, FormCheck, Badge } from 'react-bootstrap';
 import { getUserList, selectRow } from '../../ducks/users';
 import { connect } from 'react-redux';
 
-const UsersList = ({list, loading, getUserList, selectRow}) => {
+const UsersList = ({list, loading, getUserList, selectRow,selected}) => {
     useEffect(() => {
         getUserList()
     }, [])
@@ -11,30 +11,38 @@ const UsersList = ({list, loading, getUserList, selectRow}) => {
     return (
         <>
         {!loading && list.length > 0 &&
-            <Table >
-                <thead className='bg-warning text-white'>
+            <Table size='sm'>
+                <thead className='bg-warning text-white text-center text-uppercase'>
                     <tr>
                         <th><FormCheck type='checkbox' /></th>
-                        <th>ID</th>
                         <th>Razon Social</th>
-                        <th>CI/RIF</th>
-                        <th>Ubicacion</th>
-                        <th>Ancho de Banda</th>
-                        <th>Direccion IP</th>
+                        <th>CI/RIF </th>
+                        <th>Email</th>
+                        <th>Localizacion </th>
+                        <th>Telefono </th>
+                        <th>Mensualidad</th>
+                        <th>MB</th>
+                        <th>Direccion IP </th>
+                        <th>Serial </th>
+                        <th>Direccion MAC </th>
                         <th>Servicio</th>
-                        <th>Status</th>
+                        <th>STATUS</th>
                     </tr>
                 </thead>
-                <tbody className='text-center'>
+                <tbody className='text-center' >
                     { list.map(user => (
-                        <tr>
-                            <td><FormCheck type='checkbox' onChange={({ target }) => selectRow( target.checked, user )} /></td>
-                            <td>{user.id}</td>
+                        <tr className='hover-table' onClick={()=>selectRow(!document.getElementById('select_row_'+user.id).checked,user)} >
+                            <td><FormCheck checked={selected.find(x=>x.id===user.id)} id={'select_row_'+user.id} type='checkbox' onChange={({ target }) => selectRow( target.checked, user )} /></td>
                             <td>{user.name}</td>
-                            <td>{user.document}</td>
-                            <td>{user.location}</td>
+                            <td>{user.document} </td>
+                            <td>{user.email}</td>
+                            <td>{user.location} </td>
+                            <td>{user.phone}</td>
+                            <td>{user.mensualidad}</td>
                             <td>{user.bandwidth}</td>
-                            <td>{user.ip}</td>
+                            <td>{user.ip} </td>
+                            <td>{user.serial}</td>
+                            <td>{user.mac}</td>
                             <td>{user.service}</td>
                             <td><Badge variant={user.status === 'Activo' ? 'success' : user.status === 'Suspendido' ? 'warning' : 'danger'} className='text-uppercase'>{user.status}</Badge></td>
                         </tr>
@@ -50,7 +58,8 @@ const UsersList = ({list, loading, getUserList, selectRow}) => {
 const MSTP = state => (
     {
         list: state.users.list,
-        loading: state.users.loading
+        loading: state.users.loading,
+        selected:state.users.selected
     }
 )
 
